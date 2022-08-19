7a8d25453e096523570ef3d59d35347301b0a472
Hemalatha Nagarajan <citv255@nus.edu.sg>
2022-08-19 11:58:56 +0800

    simple form commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { SuccessPopupComponent } from './success-popup/success-popup.component';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,62 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FormWithValidations';
+
+  simpleForm!: FormGroup;
+  image:any = [];
+  isAddPhoto:boolean = false;
+  constructor(
+    public dialog: MatDialog
+  ) {
+    
+    this.initForms();
+    this.simpleForm.get('')
+    
 }
+  initForms() {
+    this.simpleForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName :  new FormControl('', Validators.required),
+      description : new FormControl('', Validators.required),
+      emailAddress :  new FormControl('', [Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      save : new FormControl()
+
+  })  }
+
+  get issimpleFormValid() {
+    return this.simpleForm.valid;
+  }
+
+  get borderbordercolor()
+  {
+    if(this.simpleForm.get('emailAddress')?.hasError('pattern'))
+    {
+      return "#ff0000";
+      
+    }
+    else{
+      return "#b4b4b4";
+    }
+  }
+  
+  onFileSelected(event: any)
+  {
+    var reader = new FileReader();
+		reader.readAsDataURL(event.target.files[0]);
+    reader.onload = (_event) => {
+			this.image.push(reader.result); 
+      this.isAddPhoto = true;
+		}
+   
+    }
+
+    OnSave()
+    {
+      let dialogRef = this.dialog.open(SuccessPopupComponent, {
+      
+        panelClass : 'dialog-css'
+      });
+    }
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
+import { SuccessPopupComponent } from './success-popup/success-popup.component'
+import {MatDialogModule} from '@angular/material/dialog'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessPopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatDialogModule,
+    BrowserAnimationsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
